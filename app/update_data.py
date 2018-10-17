8401ca3c9d99cb60fa9dc6b8d3d6de407f1f3b1f
ezsiyji <siyuan.jia@ericsson.com>
2018-10-17 16:57:59 +0800

    update the data and remove the xlsx

--- app/update_data.py
+++ app/update_data.py
@@ -1,50 +1,85 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
-import xlrd
 import json
 import os
-execl_path = "data.xlsx"
+
 md_path_dir = "../education/meetings/2018.10-2018.03/"
-MEETING_COUNT = 103
-MEETING_THEME = "Balance"
-MEETING_DATE = "2018.08.24"
-TableTopic = "A,B,C,D"
-BestTableTopicSpeech = "A"
-BestPreparedSpeech = "D"
-BestEvaluator = "E"
-AttendanceName = "Siyuan Jia,A"
+MEETING_COUNT = 106
+MEETING_THEME = "The Mid-Autumn Festival"
+MEETING_DATE = "2018.09.21"
+TableTopic = "Xin Feng, Jing Shao, Dongchen Tang, Jony Zheng"
+PERPARED_SPEACKERS = [{
+        "project_rank": "",
+        "people_name": "",
+        "project_name": ""
+    },
+    {
+        "project_rank": "",
+        "people_name": "",
+        "project_name": ""
+    },
+    {
+        "project_rank": "",
+        "people_name": "",
+        "project_name": ""
+    },
+    {
+        "project_rank": "",
+        "people_name": "",
+        "project_name": ""
+    },
+    {
+        "project_rank": "",
+        "people_name": "",
+        "project_name": ""
+    },
+    {
+        "project_rank": "",
+        "people_name": "",
+        "project_name": ""
+    }
+]
+BestTableTopicSpeech = "Dongchen Tang"
+BestPreparedSpeech = "Michelle Jin"
+BestEvaluator = "Elvis Jiang"
+ROLE_TAKERS = {
+    "TMD": "",
+    "TTM": "",
+    "GE": "",
+    "IE": "",
+    "Grammarian": "",
+    "Timer": "",
+    "Ah-counter": ""
+}
+AttendanceName = "Siyuan Jia,Dongchen Tang,Yi Fang,Taowen Zhang,Elvis Jiang,Michelle Jin,Jun Liu,Wujie Zhang,Xin Feng,Nrapendra Singh, Yayun Sun, Anne,Jony Zheng,Michelle Wang"
 
 
 template_attendance_for_create_html = """<h1>Attendance</h1>
 <table class="table table-condensed table-bordered">
 <tr><th>Name</th></tr>
-<tr><th>Davis Zhang</th></tr>
-<tr><th>Jason Xu</th></tr>
+<tr><th>Jenny Yu</th></tr>
+<tr><th>Huihui Jiang</th></tr>
 <tr><th>Jony Zheng</th></tr>
-<tr><th>Kuan Li</th></tr>
 <tr><th>Michelle Wang</th></tr>
-<tr><th>Yayun Sun</th></tr>
 <tr><th>Dongchen Tang</th></tr>
 <tr><th>Michelle Hua</th></tr>
 <tr><th>Jane Gong</th></tr>
 <tr><th>Taowen Zhang</th></tr>
-<tr><th>Eve Zhang</th></tr>
 <tr><th>Yi Fang</th></tr>
 <tr><th>Hongyu Qi</th></tr>
 <tr><th>Qingzhen Deng</th></tr>
-<tr><th>Jun Liu</th></tr>
 <tr><th>Siyuan Jia</th></tr>
-<tr><th>Minming Lu</th></tr>
 <tr><th>Michelle Jin</th></tr>
 <tr><th>Alice Huang</th></tr>
 <tr><th>Xin Feng</th></tr>
 <tr><th>Sarah Zhang</th></tr>
-<tr><th>Haoming Zheng</th></tr>
 <tr><th>Wujie Zhang</th></tr>
 <tr><th>Yao Fang</th></tr>
-<tr><th>Mingyu Li</th></tr>
 <tr><th>Elvis Jiang</th></tr>
+<tr><th>Nrapendra Singh</th></tr>
+<tr><th>Hong Wang</th></tr>
+<tr><th>Jun Liu</th></tr>
 <tr><th>Total</th></tr>
 </table>      
 """
@@ -52,9 +87,8 @@ template_attendance_for_create_html = """<h1>Attendance</h1>
 
 
 class Agenda:
-    def __init__(self, md_path, execl_path):
+    def __init__(self, md_path):
         self.md_path = md_path
-        self.execl_path = execl_path
         self.create_new_record()
         self.md_type_data = {
             "date": MEETING_DATE,
@@ -68,79 +102,12 @@ class Agenda:
                 "BPS": BestPreparedSpeech,
                 "BE": BestEvaluator
             },
-            "role_takers": {
-                "TMD": "",
-                "TTM": "",
-                "GE": "",
-                "IE": "",
-                "Grammarian": "",
-                "Timer": "",
-                "Ah-counter": ""
-            },
+            "role_takers": ROLE_TAKERS,
             "speakers": {
                 "TT": TableTopic,
-                "project_rank": [],
-                "people_name": [],
-                "project_name": []
+                "perpared_speackers": PERPARED_SPEACKERS,
             }
         }
-
-    def get_data(self):
-        data = xlrd.open_workbook(self.execl_path)
-        table = data.sheets()[0]
-        nrows = table.nrows
-        for i in range(nrows):
-            if i == 0:
-                continue
-            # print (table.row_values(i)[:4])
-            # print(table.row_values(0)[0], table.row_values(0)[1], table.row_values(0)[2])
-            # print(table.row_values(i)[1], "------", table.row_values(i)[2], "-----------------", i)
-            if table.row_values(i)[2] == "":                                    # nothing
-                continue
-            if table.row_values(i)[2].find("[") == -1:
-                continue
-            if table.row_values(i)[2].split("[")[1].find(",") != -1:            # title cc/cl
-                if table.row_values(i)[2].split(':')[0] == "Speaker":
-                    if table.row_values(i)[1].find('-') == -1:                  # sharing Session
-
-                        print(table.row_values(i)[1].split('(')[0], "--1--",
-                              table.row_values(i)[2].split("[")[1].split(",")[0])
-                    else:
-                        print(table.row_values(i)[1].split('(')[0], "--2--",
-                              table.row_values(i)[1].split(')')[1].split('-')[1],
-                              table.row_values(i)[2].split("[")[1].split(",")[0])
-
-                        self.md_type_data["speakers"]["project_rank"].append(table.row_values(i)[1].split('(')[0])
-                        self.md_type_data["speakers"]["project_name"].append(table.row_values(i)[1].split(')')[1].split('-')[1])
-                        self.md_type_data["speakers"]["people_name"].append(table.row_values(i)[2].split("[")[1].split(",")[0])
-                else:
-                    print(table.row_values(i)[2].split(':')[0], "--3--", table.row_values(i)[2].split("[")[1].split(",")[0])
-                    if table.row_values(i)[2].split(':')[0] in self.md_type_data["role_takers"].keys():
-                        if table.row_values(i)[2].split(':')[0] == "IE":
-                            self.md_type_data["role_takers"][table.row_values(i)[2].split(':')[0]] += \
-                                table.row_values(i)[2].split("[")[1].split(",")[0] + ","
-                        else:
-                            self.md_type_data["role_takers"][table.row_values(i)[2].split(':')[0]] = table.row_values(i)[2].split("[")[1].split(",")[0]
-            else:
-                if table.row_values(i)[2].split(':')[0] == "Speaker":
-                    if table.row_values(i)[1].find('-') == -1:
-                        print(table.row_values(i)[1].split('(')[0], "--4--",
-                              table.row_values(i)[2].split("[")[1].split("]")[0])
-                    else:
-                        print(table.row_values(i)[1].split('(')[0], "--5--",
-                              table.row_values(i)[1].split(')')[1].split('-')[1],
-                              table.row_values(i)[2].split("[")[1].split("]")[0])
-                        self.md_type_data["speakers"]["project_rank"].append(table.row_values(i)[1].split('(')[0])
-                        self.md_type_data["speakers"]["project_name"].append(table.row_values(i)[1].split(')')[1].split('-')[1])
-                        self.md_type_data["speakers"]["people_name"].append(table.row_values(i)[2].split("[")[1].split("]")[0])
-                else:
-                    print(table.row_values(i)[2].split(':')[0], "--6--", table.row_values(i)[2].split("[")[1].split("]")[0])
-                    if table.row_values(i)[2].split(':')[0] in self.md_type_data["role_takers"].keys():
-                        if table.row_values(i)[2].split(':')[0] == "IE":
-                            self.md_type_data["role_takers"][table.row_values(i)[2].split(':')[0]] += \
-                                table.row_values(i)[2].split("[")[1].split("]")[0] + ","
-                        else:
-                            self.md_type_data["role_takers"][table.row_values(i)[2].split(':')[0]] = table.row_values(i)[2].split("[")[1].split("]")[0]
 
     def save_json(self):
         print(json.dumps(self.md_type_data, indent=4))
@@ -156,11 +123,11 @@ class Agenda:
                                                              self.md_type_data["date"],
                                                              self.md_type_data["speakers"]["TT"])
 
-        for i in range(len(self.md_type_data["speakers"]["project_rank"])):
-            add_content = add_content + "`{0}` {1}-{2}   \n".format(self.md_type_data["speakers"]["project_rank"][i].strip(),
-                                                                 self.md_type_data["speakers"]["people_name"][i],
-                                                                 self.md_type_data["speakers"]["project_name"][i])
-
+        for i in range(len(self.md_type_data["speakers"]["perpared_speackers"])):
+            if self.md_type_data["speakers"]["perpared_speackers"][i]["project_rank"].strip() != "":
+                add_content = add_content + "`{0}` {1}-{2}   \n".format(self.md_type_data["speakers"]["perpared_speackers"][i]["project_rank"].strip(),
+                                                                     self.md_type_data["speakers"]["perpared_speackers"][i]["people_name"],
+                                                                     self.md_type_data["speakers"]["perpared_speackers"][i]["project_name"])
         print(add_content)
         with open(os.path.join(self.md_path, "speakers.md"), "a+") as f:
             f.writelines("\n" + add_content)
@@ -217,9 +184,10 @@ class Agenda:
         # print(soup.prettify())
         # print(soup.table.contents)
         temp_n = len(soup.find_all('tr'))
+        # print(temp_n, "nnnnnnnnnnnn")
         temp_head = []
         name = []
-        data = [[] for i in range( temp_n -1)]
+        data = [[] for i in range(temp_n - 1)]
         for i, child_tr in enumerate(soup.find_all('tr')):
             # print("!!")
             # print(child_tr)
@@ -232,10 +200,17 @@ class Agenda:
                         name.append(child_th.string)
 
                 # print(child_th.string)
-                #
-                # print("-----")
-        print(temp_head)
-        print(data)
+
+        #         print("-----")
+        # print(temp_head)
+        # print(data)
+
+        for i in data:
+            sum = 0
+            for j in i[1:] :
+                if j !=0 :
+                    sum += int(j)
+            print(i[0:1] , sum, i)
 
         #  TODO up date the temp_head and data
         if self.md_type_data["date"] not in temp_head:              # new record
@@ -259,12 +234,13 @@ class Agenda:
                 if a_name != "":
                     print(i, a_name)
                     temp_list = []
+                    temp_list.append(str(a_name))
                     for j in range(len(temp_head)-2):                        #don't need to include "name" and "1"
                         temp_list.append(0)
                         pass
                     temp_list.append(1)                                       # flag 1 , meaning the attended
                     data[len(data)-1][len(temp_head) - 1] += 1                # total + 1
-                    data.insert(temp_n-2+i, list(a_name.strip()) + temp_list)
+                    data.insert(temp_n-2+i, temp_list)
         else:                                                        # update the record
             pass                                                     # Avoid unnecessary changes not supported it
 
@@ -287,6 +263,7 @@ class Agenda:
         print(html_content)
         with open(os.path.join(self.md_path, "attendance.html"), "w") as f:
             f.writelines(html_content)
+            print("write....")
 
     def create_new_record(self):
         print(os.path.exists(self.md_path))
@@ -297,11 +274,10 @@ class Agenda:
                 f.writelines(template_attendance_for_create_html)
 
 if __name__ == '__main__':
-    ag = Agenda(md_path_dir, execl_path)
-    ag.get_data()
-    # ag.save_json()
+    ag = Agenda(md_path_dir)
+    ag.create_new_record()
+    ag.save_json()
     ag.save_speakers()
     ag.save_best_awards()
     ag.save_role_takers()
     ag.save_attendance()
-    # ag.create_new_record()
